812c218d998a1e391677b00f9cd637ce7bd4ee47
Deepyash <158832695+yashLGN@users.noreply.github.com>
2026-02-08 00:33:46 +0530

    some minor change again

--- back-propogation-default.py
+++ back-propogation-default.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Example inputs, weights, and bias for a multi-neuron layer
+# Example inputs, weights, and bias for a multiple neuron layer
 weights = np.array([[0.1 , 2.0 , 0.3 , 0.4],
                    [0.5 , 0.6 , 0.7 , 0.8],
                    [0.9 , 1.0 , 1.1 , 1.2]])
